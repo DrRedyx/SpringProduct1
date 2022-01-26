73fdc32fac76f6f4a97a6421719aed6c23bc5a1e
Redyx <dr.redyx@gmail.com>
2022-01-27 01:04:45 +0300

    Initial commit

---
+++ src/main/java/com/example/SpringProduct1/SpringProduct1Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringProduct1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProduct1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProduct1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringProduct1/SpringProduct1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringProduct1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProduct1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
